5afc6e95ffcaaed4727ad6d34b4d0b801d239169
Cameron Kingsbury <camsbury7@gmail.com>
2023-03-08 17:10:14 -0500

    run bash commands in tmux via clojure

--- src/rep_gen/core.clj
+++ src/rep_gen/core.clj
@@ -2,8 +2,9 @@
   (:require
    [camel-snake-kebab.core :as csk]
    [clojure.data.json :as json]
-   [clojure.java.io :as io]))
-
+   [clojure.java.io :as io]
+   [clojure.java.shell :refer [sh]]
+   [clojure.string :as str]))
 
 (defn from-json [raw]
   (json/read-str raw :key-fn (comp csk/->kebab-case keyword)))
@@ -11,8 +12,6 @@
 (defn get-in-tree
   [tree moves]
   (reduce (fn [mt move] (->> mt :node-responses (filter #(= move (first %))) first second)) tree moves))
-
-
 
 (def move-tree
   (->> "move-tree.json"
@@ -26,5 +25,42 @@
        slurp
        json/read-str))
 
+(defn project-name
+  []
+  (last
+   (str/split
+    (str/trim-newline
+     (:out
+      (sh "pwd")))
+    #"/")))
+
+(defn project-pid
+  []
+  (str/trim-newline
+   (:out
+    (sh "tmux" "list-panes" "-t" (project-name) "-F" "#{pane_pid}"))))
+
+(defn run-in-tmux
+  [cmd]
+  (sh
+   "bash"
+   "-c"
+   (str
+    cmd
+    " 1> /proc/"
+    (project-pid)
+    "/fd/1 2> /proc/"
+    (project-pid)
+    "/fd/2")))
+
+
+;; figure out how to turn edn into json and send to the following command
+;; cabal run rep-gen -- -c <CONFIG>
+;; then redirect the stdout and stderr (ostensibly just through tee) to the file descriptors pulled by the session name
+
 (comment
+  (run-in-tmux "echo hi")
+
   )
+
+
